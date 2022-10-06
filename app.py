5360d4ad79bb0160b80b36b3b68421695ed2ee90
okrupko-amplify <108939664+okrupko-amplify@users.noreply.github.com>
2022-10-07 00:35:08 +0300

    Refactoring a bit

--- app.py
+++ app.py
@@ -8,20 +8,19 @@ from flask_cors import CORS
 
 # config
 sys.dont_write_bytecode = True  # avoid writing __pycache__ and .pyc
-SQLALCHEMY_TRACK_MODIFICATIONS = True
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://krupko_space:IGNOMINY-envisage-godly@192.168.0.155:54327/krupko_space'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://krupko_space:sterhedsg45whes@192.168.0.158:54321/krupko_space'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
-CORS(app)
 
 # database
+db = SQLAlchemy(app)
 import models.feeds
 db.create_all()
 
 # routes
+CORS(app)
 import routes.feeds
 
 # run app
