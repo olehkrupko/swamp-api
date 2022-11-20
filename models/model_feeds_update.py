3433c4ab9b1fc682f1511d50a60a35d193a4bc43
okrupko-amplify <108939664+okrupko-amplify@users.noreply.github.com>
2022-11-20 14:44:31 +0200

    FeedUpdate model updated

--- models/model_feeds_update.py
+++ models/model_feeds_update.py
@@ -9,17 +9,50 @@ class FeedUpdate(db.Model):
     feed_id  = db.Column(db.Integer,  db.ForeignKey("feed.id"), nullable=False)
     created  = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     # core/required
-    title    = db.Column(db.String(100), unique=False,          nullable=False)
+    name     = db.Column(db.String(100), unique=False,          nullable=False)
     href     = db.Column(db.String(200), unique=True,           nullable=False)
     datetime = db.Column(db.DateTime,    default=None)
 
     def __init__(self, data: dict):
         if not isinstance(data, dict):
-            raise Exception(f"__init__ data {data} has to be a dict")
+            raise Exception(f"__init__ data {data} has to be a dict, not {type(data)}")
 
-        self.title = data.pop('title')
+        # # # FILTERING: passing item cycle if filter does not match
+        # # if self.filter is not None:
+        # #     if each.name.find(self.filter) == -1 and each.href.find(self.filter) == -1:
+        # #         continue
+
+        # # DATETIME fixes: fix timezone unaware
+        # if each.datetime.tzinfo is not None and each.datetime.tzinfo.utcoffset(each.datetime) is not None:
+        #     each_dt = localtime(each.datetime)
+        #     each.datetime = datetime(
+        #         each_dt.year,
+        #         each_dt.month,
+        #         each_dt.day,
+        #         each_dt.hour,
+        #         each_dt.minute,
+        #         each_dt.second
+        #     )
+
+        # # NAME fixes
+        # each.name = ' '.join(each.name.split())
+        # each.name = each.name.strip()
+        # # # extra symbols
+        # # if each.title == 'Shadman':
+        # #     each.name = each.name[:each.name.find('(')-1]
+        # # elif each.title == 'Apple' and each.name[-len('Apple'):] == 'Apple':
+        # #     # - symbol can be a variety of different symbols
+        # #     # 8 = len(' - Apple')
+        # #     each.name = each.name[:-8]
+        # # elif each.title == 'LastWeekTonight':
+        # #     end = each.name.find(': Last Week Tonight with John Oliver (HBO)')
+        # #     if end != -1:
+        # #         each.name = each.name[:end]
+
+        self.name = data.pop('name')
         self.href = data.pop('href')
         self.datetime = data.pop('datetime')
+        self.feed_id = data.pop('feed_id')
 
         if data:
             raise Exception(f"Dict {data} has extra data")
@@ -27,9 +60,13 @@ class FeedUpdate(db.Model):
     def as_dict(self):
         return {
             'id': self.id,
+            'feed_id': self.feed_id,
             'created': self.created,
 
-            'title': self.title,
+            'name': self.name,
             'href': self.href,
             'datetime': self.datetime,
         }
+    
+    def __str__(self):
+        return str(self.as_dict())
