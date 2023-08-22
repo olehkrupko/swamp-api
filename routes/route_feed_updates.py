232350b8c16acee6e7b7373f299f213369cc063f
Oleh Krupko <oleh.krupko@gmail.com>
2023-08-22 11:09:33 +0300

    Introducing filter limit

--- routes/route_feed_updates.py
+++ routes/route_feed_updates.py
@@ -10,12 +10,13 @@ from models.model_feeds_update import FeedUpdate
 ROUTE_PATH = "/feed-updates"
 
 
-@app.route(f"{ ROUTE_PATH }/", methods=['GET'])
+@app.route(f"{ ROUTE_PATH }/", methods=['GET'], defaults={'limit': 140})
+@app.route(f"{ ROUTE_PATH }/<limit>", methods=['GET'])
 @cross_origin(headers=['Content-Type']) # Send Access-Control-Allow-Headers
-def list_feed_updates():
+def list_feed_updates(limit):
     return app.response_class(
         response=json.dumps({
-            "response": db.session.query(FeedUpdate).filter_by(**request.args).first().as_dict(),
+            "response": [x.as_dict() for x in db.session.query(FeedUpdate).filter_by(**request.args).limit(limit).all()],
         }, default=str),
         status=200,
         mimetype='application/json'
