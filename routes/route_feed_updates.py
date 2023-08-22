3fadf9c7317c550a1ae6bd866bcbca890d34f46f
Oleh Krupko <oleh.krupko@gmail.com>
2023-08-22 21:28:10 +0300

    Limit is part of ? args now

--- routes/route_feed_updates.py
+++ routes/route_feed_updates.py
@@ -11,10 +11,15 @@ from models.model_feeds_update import FeedUpdate
 ROUTE_PATH = "/feed-updates"
 
 
-@app.route(f"{ ROUTE_PATH }/", methods=['GET'], defaults={'limit': 140})
-@app.route(f"{ ROUTE_PATH }/<limit>", methods=['GET'])
+@app.route(f"{ ROUTE_PATH }/", methods=['GET'])
 @cross_origin(headers=['Content-Type']) # Send Access-Control-Allow-Headers
-def list_feed_updates(limit):
+def list_feed_updates():
+    kwargs = request.args
+    if limit in kwargs:
+        limit = kwargs.pop(limit)
+    else:
+        limit = 140
+
     return shared.return_json(
-        response=[x.as_dict() for x in db.session.query(FeedUpdate).filter_by(**request.args).limit(limit).all()]
+        response=[x.as_dict() for x in db.session.query(FeedUpdate).filter_by(**kwargs).limit(limit).all()]
     )
