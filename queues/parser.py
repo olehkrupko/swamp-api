04582542c6964ce33022d02a0e508a79872b704a
Oleh Krupko <oleh.krupko@gmail.com>
2023-12-14 00:02:26 +0200

    Added feed push queue support

--- queues/parser.py
+++ queues/parser.py
@@ -5,8 +5,24 @@ from models.model_feeds import Feed
 
 
 # @rabbit.queue(routing_key="feed.parser", exchange_type=ExchangeType.DIRECT)
-# def feed_parser(routing_key, body):
+# def queue_feed_parser(routing_key, body):
 #     Feed.process_parsing(
 #         feed_id=body["_id"],
 #         store_new=True,
 #     )
+
+
+@rabbit.queue(routing_key="feed.push", exchange_type=ExchangeType.DIRECT)
+def queue_feed_push(routing_key, body):
+    feed = (
+        db.session.query(Feed)
+        .filter_by(
+            _id=body["_id"],
+        )
+        .first()
+    )
+
+    new_updates = feed.ingest_updates(
+        body["updates"]
+    )
+    print(f"---> { len(new_updates) }")
