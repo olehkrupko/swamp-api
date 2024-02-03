b7e55b0f56681269ae12f83b9a25a8619af3de5e
Oleh Krupko <oleh.krupko@gmail.com>
2024-02-03 10:42:13 +0200

    Update model_feeds.py

--- models/model_feeds.py
+++ models/model_feeds.py
@@ -121,7 +121,7 @@ class Feed(db.Model):
         return results
 
     def ingest_updates(self, updates):
-        updates.sort(key=lambda x: x["datetime"], reverse=False)
+        updates.sort(key=lambda x: x["datetime"], reverse=True)
         for each in updates:
             each["feed_id"] = self._id
         if "limit" in self.json and isinstance(self.json["limit"], int):
