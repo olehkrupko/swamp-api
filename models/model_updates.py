079b102b7259e3f88eb1fd1a4c5477b1d4219173
Oleh Krupko <oleh.krupko@gmail.com>
2024-03-14 01:23:36 +0200

    Telegram: update message

--- models/model_updates.py
+++ models/model_updates.py
@@ -139,8 +139,8 @@ class Update(db.Model):
             )
 
         message = (
-            f"{self.name}\n\n"
-            f"[[OPEN]]({self.href}) [[EDIT]](http://192.168.0.155:30011/feeds/{self.feed_id}/edit)"
+            f"{telegram.helpers.escape_markdown(self.name)}\n\n"
+            f"\[[OPEN]({self.href})] - \[[EDIT](http://192.168.0.155:30011/feeds/{self.feed_id}/edit)]"
         )
 
         asyncio.run(
