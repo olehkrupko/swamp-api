6b2c34883fb6e9a534a6144e310d2479575de99b
Oleh Krupko <oleh.krupko@gmail.com>
2024-03-19 18:22:12 +0200

    Telegram: formatting update data

--- services/service_telegram.py
+++ services/service_telegram.py
@@ -10,6 +10,7 @@ import telegram
 class TelegramService:
     CHAT_ID = os.environ.get("TELEGRAM_BOT_DMS")
     TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+    PARSE_MODE = "markdown"
     MESSAGE_MARKDOWN = (
         "{name}\n"
         "\n"
@@ -19,17 +20,17 @@ class TelegramService:
     @classmethod
     async def send_message(cls, msg):
         await telegram.Bot(cls.TOKEN).sendMessage(
+            parse_mode=cls.PARSE_MODE,
             chat_id=cls.CHAT_ID,
             text=msg,
-            parse_mode="markdown",
         )
 
     @classmethod
     def send_update(cls, update):
         message = cls.MESSAGE_MARKDOWN.format(
             name=telegram.helpers.escape_markdown(update.name),
-            href=update.href,
-            feed_id=update.feed_id,
+            href=telegram.helpers.escape_markdown(update.href),
+            feed_id=str(int(update.feed_id)),
         )
 
         asyncio.run(
