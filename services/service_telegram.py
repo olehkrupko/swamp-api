16ff05cf1849bf14b19d311a4874f28a1a431394
Oleh Krupko <oleh.krupko@gmail.com>
2024-03-25 23:18:39 +0200

    Refactoring: remove outdated code

--- services/service_telegram.py
+++ services/service_telegram.py
@@ -3,9 +3,6 @@ import os
 
 import telegram
 
-# from telegram import Update
-# from telegram.ext import Application, ContextTypes, CommandHandler
-
 
 class TelegramService:
     CHAT_ID = os.environ.get("TELEGRAM_BOT_DMS")
@@ -38,29 +35,3 @@ class TelegramService:
                 message,
             )
         )
-
-
-# async def bot_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await context.bot.send_message(
-#         chat_id=update.effective_chat.id,
-#         text="I'm swamp-courier bot.",
-#     )
-
-
-# def bot_builder():
-#     app = (
-#         Application.builder()
-#         .token(os.environ.get("TELEGRAM_BOT_TOKEN"))
-#         .read_timeout(30)
-#         .write_timeout(30)
-#         .build()
-#     )
-
-#     app.add_handler(CommandHandler("start", bot_start))
-
-#     app.run_polling()
-#     app.idle()
-
-
-# # not imported as there is no need in it for now
-# bot_builder()
