df778664c49be2b57a33ecbeb472ab2237e17b07
Oleh Krupko <oleh.krupko@gmail.com>
2024-04-22 13:14:01 +0300

    Updates: __init__() refactored

--- models/model_updates.py
+++ models/model_updates.py
@@ -49,13 +49,15 @@ class Update(db.Model):
     # RELATIONSHIPS
     feed: Mapped["Feed"] = relationship(back_populates="updates")
 
-    def __init__(self, data: dict):
-        data = data.copy()
-        if not isinstance(data, dict):
-            raise Exception(f"{type(data)} {data=} has to be a dict")
-
-        name = data.pop("name")
-        # transforming emojis to normal words:
+    def __init__(
+        self,
+        name: str,
+        datetime: str,
+        href: str,
+        feed_id: int = None,
+    ):
+        # name
+        # transforming emojis to normal words:s
         name = emoji.demojize(name, delimiters=(" ", " "))
         name = name.replace("#", " ")  # removing hashtags
         name = name.replace("_", " ")  # underscores are just weird spaces
@@ -68,27 +70,26 @@ class Update(db.Model):
             # name = f"No name in update by { feed_title }"
             name = "No name in update by {feed_title}"
 
-        datetime_event = data.pop("datetime")
-        if isinstance(datetime_event, str):
-            datetime_event = datetime.datetime.fromisoformat(datetime_event)
-        if datetime_event.tzinfo:
+        # datetime
+        if isinstance(datetime, str):
+            datetime = dt.datetime.fromisoformat(datetime)
+        else:
+            raise ValueError("Update.__init__() datetime is expected to be str")
+        if datetime.tzinfo:
             # if tzinfo present — convert to current one
-            datetime_event = datetime_event.astimezone(
+            datetime = datetime.astimezone(
                 ZoneInfo(os.environ.get("TIMEZONE_LOCAL"))
             )
         else:
             # if no tzinfo — replace it with current one
-            datetime_event = datetime_event.replace(
+            datetime = datetime.replace(
                 tzinfo=ZoneInfo(os.environ.get("TIMEZONE_LOCAL"))
             )
 
         self.name = name[:140]
-        self.href = data.pop("href")
-        self.datetime = datetime_event
-        self.feed_id = data.pop("feed_id")
-
-        if data:
-            raise ValueError(f"Dict {data=} has extra data")
+        self.href = href[:300]
+        self.datetime = datetime
+        self.feed_id = feed_id
 
     def as_dict(self):
         return {
