79099e1a3fecffecf00d78a002d52bede53ed96e
Oleh Krupko <oleh.krupko@gmail.com>
2025-05-20 17:13:58 +0300

    Config(Settings): removed deprecated sections

--- config/config.py
+++ config/config.py
@@ -4,10 +4,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    # SQLALCHEMY_TRACK_MODIFICATIONS: bool = True
     SQLALCHEMY_DATABASE_URI: str = getenv("SQLALCHEMY_DB_URI")
-    # SQLALCHEMY_ECHO = True
-    # SCHEDULER_API_ENABLED = True
 
 
 # import this one:
