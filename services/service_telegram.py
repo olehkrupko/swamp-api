8b09372cb5b60546e78a273916baf421777095e1
Oleh Krupko <oleh.krupko@gmail.com>
2026-05-20 12:11:48 +0200

    Refactoring: apply suggestion(s)

--- services/service_telegram.py
+++ services/service_telegram.py
@@ -14,6 +14,7 @@ class TelegramService:
 
     Sends single messages and bulk feed update notifications through Telegram.
     """
+
     PARSE_MODE = "markdown"
     MESSAGE_MARKDOWN = (
         "{name}\n"
